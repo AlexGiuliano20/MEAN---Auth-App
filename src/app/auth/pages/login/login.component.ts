95418119b26cc0444c5212203f770efe61072d7c
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-09-19 02:54:18 -0300

    Mensaje de error con sweet alert

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
+
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -32,8 +35,11 @@ export class LoginComponent {
     const { email, password } = this.miFormulario.value;
 
     this._authService.login(email, password).subscribe((ok) => {
-      if (ok) {
+      console.log(ok);
+      if (ok === true) {
         this._route.navigateByUrl('/dashboard');
+      } else {
+        Swal.fire('Error', ok, 'error');
       }
     });
   }
